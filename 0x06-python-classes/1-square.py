ceddb37f38596f1c066db14577407aba59b9f93b
dannyamuche <preciousamuche@gmail.com>
2023-01-28 00:40:23 +0000

    Add All

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Danny Amuche
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined
+        """
+
+        self.__size = size
